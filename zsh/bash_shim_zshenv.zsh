0877e3acae6b6357330522645745e3004460ba3f
Joe Castro <joecastro@google.com>
2024-07-12 17:21:37 -0700

    Removing bash overrides

--- zsh/bash_shim_zshenv.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-#pragma once
-
-test -e ~/.env_vars.sh && source ~/.env_vars.sh
--- zsh/bash_shim_zshrc.zsh
+++
@@ -1,9 +0,0 @@
-#! /bin/zsh
-
-# pragma-once
-
-# This could be limited to interactive shells if desired.
-# `if echo $- | grep -q 'i'; then`
-# Or, this could be limited to verifying extra qualities about the bash executable like a minimum version.
-
-SHELL=/bin/bash exec /bin/bash $@
