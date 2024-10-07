737408f27f1b121f50ca3b6c494d6b46ede54268
Joe Castro <joecastro@google.com>
2024-10-07 10:44:20 -0700

    More robust installation of nerdfonts in bootstrap script

--- bootstrap_chromebook.sh
+++ bootstrap_chromebook.sh
@@ -31,17 +31,25 @@ sudo apt install -y \
     python3-dev \
     python3.11-venv
 
-mkdir -p ~/.fonts
-pushd ~/.fonts || return 1
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFontMono-Regular.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Bold/JetBrainsMonoNerdFont-Bold.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/BoldItalic/JetBrainsMonoNerdFont-BoldItalic.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Regular/CaskaydiaCoveNerdFontMono-Regular.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Regular/CaskaydiaCoveNerdFontMono-Italic.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Bold/CaskaydiaCoveNerdFontMono-Bold.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Bold/CaskaydiaCoveNerdFontPropo-BoldItalic.ttf
+function install_fonts() {
+    mkdir -p "$HOME/Downloads"
+    mkdir -p "$HOME/.local/share/fonts"
 
-popd || return 1
+    local font_families=("JetBrainsMono" "CascadiaCode" "Hack" "Inconsolata" "FiraCode" "UbuntuSans")
+    local nf_version
+    nf_version=$(wget -q -O - "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest" | grep "\"name\"" | head -n 1 | cut -d '"' -f 4)
 
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-dpkg -i *.deb
+    for font_family in "${font_families[@]}"; do
+        local font_url="https://github.com/ryanoasis/nerd-fonts/releases/download/${nf_version}/${font_family}.zip"
+        wget -O "$HOME/Downloads/${font_family}.zip" "${font_url}"
+        unzip -f -o -d "$HOME/.local/share/fonts" "$HOME/Downloads/${font_family}.zip" "*.ttf"
+        rm "$HOME/Downloads/${font_family}.zip"
+    done
+
+    fc-cache -f -v
+}
+
+install_fonts
+
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+dpkg -i ./*.deb
