2d17e86d11f3f6f98921eaecea156354a63953e2
Joe Castro <joecastro@google.com>
2024-11-05 21:37:47 -0800

    Fixing cache functions and aliasing ssh so I can reset Konsole profile on exit

--- shell/env_funcs.sh
+++ shell/env_funcs.sh
@@ -498,21 +498,35 @@ if __is_shell_zsh; then
         local value="$2"
         local expiration_time="${3:-0}"
 
-        Z_CACHE["${key}"]="${value}"
-        if [[ "${expiration_time}" -gt 0 ]]; then
-            Z_CACHE["${key}_expiration"]="$(( $(date +%s) + expiration_time ))"
+        Z_CACHE[${key}]="${value}"
+        if [[ ${expiration_time} -gt 0 ]]; then
+            Z_CACHE[${key}_expiration]="$(( $(date +%s) + expiration_time ))"
+        else
+            Z_CACHE[${key}_expiration]=0
         fi
+    }
+
+    function __cache_get_expiration() {
+        local key="$1"
+        local expiration_value
+        expiration_value="${Z_CACHE[${key}_expiration]}"
+        if [[ -n "${expiration_value}" ]]; then
+            echo -n "${expiration_value}"
+            return 0
+        fi
+        return 1
     }
 
     function __cache_get() {
         local key="$1"
-        local current_time
-        current_time=$(date +%s)
 
-        if [[ -n "${Z_CACHE["${key}_expiration"]}" ]]; then
-            if [[ "${Z_CACHE["${key}_expiration"]}" -lt "${current_time}" ]]; then
-                unset "Z_CACHE[${key}]"
-                unset "Z_CACHE[${key}_expiration]"
+        local cache_expiration
+        if ! cache_expiration=$(__cache_get_expiration "${key}"); then
+            return 1
+        fi
+
+        if [[ "${cache_expiration}" -gt 0 ]]; then
+            if [[ "${cache_expiration}" -lt "$(date +%s)" ]]; then
                 return 1
             fi
         fi
@@ -526,12 +540,39 @@ if __is_shell_zsh; then
     }
 
     function __cache_clear() {
-        Z_CACHE=()
+        if [[ -n "$1" ]]; then
+            Z_CACHE[${1}_expiration]=1
+        else
+            Z_CACHE=()
+        fi
     }
 
 # Prevent bash from attempting to interpret invalid zsh syntax.
 # shellcheck disable=SC1091
 source /dev/stdin <<'EOF'
+
+    function __cache_print() {
+        local expires_soon_threshold=1800
+        local current_time
+        current_time="$(date +%s)"
+        local expiration_value
+        local suffix
+        for key in "${(@k)Z_CACHE}"; do
+            if expiration_value=$(__cache_get_expiration "${key}"); then
+                if [[ "${expiration_value}" -eq 0 ]]; then
+                    suffix=""
+                elif [[ "${expiration_value}" -lt "${current_time}" ]]; then
+                    suffix=" (expired)"
+                else
+                    local remaining_time=$((expiration_value - current_time))
+                    local remaining_minutes=$((remaining_time / 60))
+                    suffix=" (expires in ${remaining_minutes} minutes)"
+                fi
+                echo "${key}=\"${Z_CACHE[$key]}\"${suffix}"
+            fi
+        done
+    }
+
     function __cache_save() {
         local cache_file="$1"
         local key
@@ -575,6 +616,10 @@ else
     }
 
     function __cache_clear() {
+        return 0
+    }
+
+    function __cache_print() {
         return 0
     }
 
@@ -656,7 +701,6 @@ else
     function __echo_colored() {
         local color_name="$1"
         shift
-        local text="$*"
         local style=""
 
         # Check for optional style parameters
@@ -664,8 +708,9 @@ else
             style="$color_name"
             color_name="$1"
             shift
-            text="$*"
         fi
+
+        local text="$*"
 
         declare -A color_map=(
             ["black"]="0"
@@ -820,7 +865,7 @@ function __cute_pwd_short() {
 function __cute_time_prompt() {
     case "$(date +%Z)" in
     UTC)
-        echo -n "$(date +'%_H:%Mz')"
+        echo -n "$(date -u +'%H:%Mz')"
         ;;
     *)
         echo -n "$(date +'%_H:%M %Z')"
@@ -863,6 +908,11 @@ if __is_tool_window; then
 fi
 
 # Shared setup helpers for bash and zsh.
+
+function ssh() {
+    __cache_clear "KONSOLE_PROFILE"
+    command ssh "$@"
+}
 
 function __do_eza_aliases() {
     # if eza is installed prefer that to ls
